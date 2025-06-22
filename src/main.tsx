2d08f32f4788890cb9ebc1d9996217a7894b5139
r74tech <r74tech@gmail.com>
2025-06-22 15:52:01 +0900

    hotfix: Switch from BrowserRouter to HashRouter for routing in main application file.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import App from "./App.tsx";
 import { store } from "./store";
 import "./styles/index.css";
@@ -9,13 +9,13 @@ import "./styles/index.css";
 ReactDOM.createRoot(document.getElementById("container-wrap-wrap") as HTMLElement).render(
 	<StrictMode>
 		<Provider store={store}>
-			<BrowserRouter>
+			<HashRouter>
 				<Routes>
 					<Route path="/" element={<App />} />
 					<Route path="/share/:shortId" element={<App />} />
 					<Route path="*" element={<App />} />
 				</Routes>
-			</BrowserRouter>
+			</HashRouter>
 		</Provider>
 	</StrictMode>,
 );
